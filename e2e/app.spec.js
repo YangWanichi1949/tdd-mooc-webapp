569879fdd291f387c2a273f69369a40ccb2750e2
YangWanichi1949 <yangcheng@moegi.waseda.j>
2026-05-12 20:25:23 +0900

    Reset todo storage before end-to-end test

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -3,6 +3,9 @@ const { test, expect } = require('@playwright/test');
 test('can add a todo', async ({ page }) => {
   await page.goto('http://localhost:8080');
 
+  await page.evaluate(() => localStorage.clear());
+  await page.reload();
+
   await page.fill('#todo-input', 'Buy milk');
 
   await page.click('#add-button');
